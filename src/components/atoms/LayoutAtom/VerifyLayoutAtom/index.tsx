34b1a509d59d3072e8f1c65f4327e75d941c1874
KienNDT <nightstealth99@gmail.com>
2023-05-27 08:55:59 +0700

    common hóa

--- src/components/atoms/LayoutAtom/VerifyLayoutAtom/index.tsx
+++ src/components/atoms/LayoutAtom/VerifyLayoutAtom/index.tsx
@@ -1,18 +1,21 @@
 import { FunctionComponent } from "react";
 import styles from "./styles.module.css";
+import clsx from "clsx";
 
 export type Props = {
   children: React.ReactNode;
   title: string;
   description: string;
+  className?: string;
 };
 
 export const VerifyLayoutAtom: FunctionComponent<Props> = (props) => {
   return (
-    <div className={styles["verifylayout"]}>
+    <div className={clsx(styles["verifylayout"], props.className)}>
       <img className={styles["verifylayout__logo"]} src={"/images/logo.png"} />
       <h3 className={styles["verifylayout__title"]}>{props.title}</h3>
       <p className={styles["verifylayout__description"]}>{props.description}</p>
+      {props.children}
     </div>
   );
 };
