b3f02b173c32b6a3b11225f3face85dc204b6173
KienNDT <nightstealth99@gmail.com>
2023-05-27 23:30:22 +0700

    tạo common giao diện quản lý

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,35 @@ import { ToastContainer } from "react-toastify";
 import "../styles/globals.css";
 import styles from "../styles/styles.module.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3699FF",
+    },
+  },
+  typography: {
+    fontFamily: "Open Sans",
+    h3: {
+      fontWeight: 700,
+      fontSize: "2.4rem",
+      lineHeight: "30px",
+    },
+    h5: {
+      fontWeight: 600,
+      fontSize: "1.6rem",
+      lineHeight: "20px",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <ToastContainer className={styles["notification__container"]} />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/project/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import LoginTemplate from "../../components/templates/login";
+import { ManageLayoutAtom } from "../../components/atoms/LayoutAtom/ManageLayoutAtom";
+
+const Project: NextPage = () => {
+  return <ManageLayoutAtom />;
+};
+
+export default Project;
