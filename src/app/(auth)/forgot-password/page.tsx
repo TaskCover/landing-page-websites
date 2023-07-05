8de3bd1645f58cd6aea876c39b102f9cee378d56
Finn <minhreal13@gmail.com>
2023-07-05 22:41:33 +0700

    remove redundant code

--- src/app/(auth)/forgot-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import ForgotPage from "components/sn-forgot-password";
-
-export const metadata = {
-  title: "Quên mật khẩu | Taskcover",
-};
-
-export default function Page() {
-  return <ForgotPage />;
-}
--- src/app/(root)/user-information/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserInformationPage from "components/sn-user-information";
-
-export const metadata = {
-  title: "Thông tin tài khoản | Taskcover",
-};
-
-export default function Page() {
-  return <UserInformationPage />;
-}
