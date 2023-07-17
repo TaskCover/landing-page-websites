01e8fc2bd3ce13b8672037218f2f9753788e2857
Finn <minhreal13@gmail.com>
2023-07-17 08:26:55 +0700

    update css

--- src/components/sn-project-detail/Tasks/ItemList.tsx
+++ src/components/sn-project-detail/Tasks/ItemList.tsx
@@ -592,7 +592,7 @@ const ItemList = () => {
         noData={!isIdle && totalItems === 0}
         display={{ xs: "none", md: "flex" }}
         position="sticky"
-        top={{ xs: baseTop + 8, xl: baseTop + 24 }}
+        top={{ xs: baseTop + 8, xl: baseTop + 42 }}
         zIndex={1}
       >
         <></>
