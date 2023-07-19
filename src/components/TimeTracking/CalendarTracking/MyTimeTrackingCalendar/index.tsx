aacbe43a96dc281fbb80e8207313d41d06ae9e08
Lại Minh Sáng <kaidev99@Kais-MacBook-Pro.local>
2023-07-19 14:23:01 +0700

    fix action search my time

--- src/components/TimeTracking/CalendarTracking/MyTimeTrackingCalendar/index.tsx
+++ src/components/TimeTracking/CalendarTracking/MyTimeTrackingCalendar/index.tsx
@@ -380,14 +380,16 @@ const TrackingCalendar: React.FC<IProps> = () => {
           >
             {activeTab === "timeSheet" && (
               <CustomizedInputBase
-                value={filters.search_key}
-                placeholder={timeT("myTime.searchButton")}
-                onChange={(event) =>
-                  setFilters({ ...filters, search_key: event.target.value })
-                }
-                // onKeyUp={(event) =>
-                //   event.key === "Enter" && onGetMyTimeSheet(filters)
-                // }
+              value={filters.search_key}
+              placeholder = { timeT("myTime.searchButton") }
+              onChange={(event) => {
+                const searchKey = event.target.value;
+                setFilters({ ...filters, search_key: searchKey });
+                onGetMyTimeSheet({
+                  ...filters,
+                  search_key: searchKey,
+                });
+              }}
               />
             )}
           </Grid>
