7a6431374731a718667727e3ad2a7093ca4cbe6c
Duy Nguyen <duy.se.it99@gmail.com>
2023-09-06 13:51:57 +0700

    feat(chatting-room): add chatbar

--- src/components/sn-chatting-room/components/RoomDetails/components/RoomHeader.tsx
+++ src/components/sn-chatting-room/components/RoomDetails/components/RoomHeader.tsx
@@ -11,45 +11,47 @@ const RoomHeader = () => {
     const { currentConversation } = useChattingActions()
     const { isDarkMode } = useTheme();
 
-    return <Box width="100%" display="flex" justifyContent="space-between" height="76px" padding="10px" bgcolor="var(--Gray0, #F7F7FD);">
-        <Box width="100%" display="flex" padding="10px" gap="20px" alignItems="center">
-            <Avatar src={currentConversation?.avatar} sx={{ height: '56px', width: "56px", borderRadius: '10px' }} />
-            <Box display="flex" flexDirection="column" gap="4px">
-                <Typography variant="h6" color="var(--Black, #212121)">{currentConversation?.name}</Typography>
-                <Typography variant="body2" color="var(--Gray3, #999)">Online</Typography>
+    return (
+        <Box width="100%" display="flex" justifyContent="space-between" height="76px" padding="10px" bgcolor="var(--Gray0, #F7F7FD);">
+            <Box width="100%" display="flex" padding="10px" gap="20px" alignItems="center">
+                <Avatar src={currentConversation?.avatar} sx={{ height: '56px', width: "56px", borderRadius: '10px' }} />
+                <Box display="flex" flexDirection="column" gap="4px">
+                    <Typography variant="h6" color="var(--Black, #212121)">{currentConversation?.name}</Typography>
+                    <Typography variant="body2" color="var(--Gray3, #999)">Online</Typography>
+                </Box>
+            </Box>
+            <Box justifyContent="flex-end" width="100%" display="flex" padding="10px" gap="4px" alignItems="center">
+                <IconButton
+                    sx={{
+                        color: colorSchemes[isDarkMode ? 'dark' : 'light'].palette.secondary.main,
+                    }}
+                >
+                    <SearchIcon />
+                </IconButton>
+                <IconButton
+                    sx={{
+                        color: 'transparent',
+                    }}
+                >
+                    <ProfileAdd />
+                </IconButton>
+                <IconButton
+                    sx={{
+                        color: 'transparent',
+                    }}
+                >
+                    <VideoCallIcon />
+                </IconButton>
+                <IconButton
+                    sx={{
+                        color: 'transparent',
+                    }}
+                >
+                    <SidebarIcon />
+                </IconButton>
             </Box>
         </Box>
-        <Box justifyContent="flex-end" width="100%" display="flex" padding="10px" gap="4px" alignItems="center">
-            <IconButton
-                sx={{
-                    color: colorSchemes[isDarkMode ? 'dark' : 'light'].palette.secondary.main,
-                }}
-            >
-                <SearchIcon />
-            </IconButton>
-            <IconButton
-                sx={{
-                    color: 'transparent',
-                }}
-            >
-                <ProfileAdd />
-            </IconButton>
-            <IconButton
-                sx={{
-                    color: 'transparent',
-                }}
-            >
-                <VideoCallIcon />
-            </IconButton>
-            <IconButton
-                sx={{
-                    color: 'transparent',
-                }}
-            >
-                <SidebarIcon />
-            </IconButton>
-        </Box>
-    </Box >
+    )
 }
 
 export default RoomHeader
