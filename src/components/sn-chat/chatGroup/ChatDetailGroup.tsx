256cd80cdb02d8341e8e16e1c156ce35fc438ef5
tienhm <tienhm@vnext.com.vn>
2023-09-14 00:27:46 +0700

    change group name

--- src/components/sn-chat/chatGroup/ChatDetailGroup.tsx
+++ src/components/sn-chat/chatGroup/ChatDetailGroup.tsx
@@ -20,8 +20,11 @@ import { useAuth, useSnackbar } from "store/app/selectors";
 import ItemDetail from "../components/ItemDetail";
 import MediaFileIconGroup from "icons/MediaFileIconGroup";
 import LinkIconGroup from "icons/LinkIconGroup";
+import { uploadFile } from "store/chat/media/actionMedia";
+import { useAppDispatch } from "store/hooks";
 
 const ChatDetailGroup = (props) => {
+  const dispatch = useAppDispatch();
   const {
     typeList,
     dataTransfer,
@@ -36,7 +39,8 @@ const ChatDetailGroup = (props) => {
     onRemoveGroupMember,
     onSetConversationInfo,
     onDeleteConversationGroup,
-    onGetAllConvention
+    onGetAllConvention,
+    onChangeGroupAvatar,
   } = useChat();
   const { user } = useAuth();
   //check owner
@@ -291,7 +295,7 @@ const ChatDetailGroup = (props) => {
       };
       const renameResult = (await onRenameGroup({
         roomId: dataTransfer?._id,
-        name: renameGroup,
+        name: renameGroup.replaceAll(' ', '_'),
       })) as any;
       
       if (renameResult?.error) {
@@ -427,6 +431,11 @@ const ChatDetailGroup = (props) => {
                   id="upload-photo"
                   name="upload-photo"
                   type="file"
+                  onChange={async (e) => {
+                    if (e.currentTarget.files?.length) {
+                      onChangeGroupAvatar(e.currentTarget.files[0], dataTransfer?._id);
+                    }
+                  }}
                 />
                 <Fab
                   color="primary"
