0d5b5f1c6f87a1a71e5cb6f037241d406018d500
DatHip <83833987+DatHip@users.noreply.github.com>
2023-10-26 12:11:39 +0700

    fix: docs dark mode & language

--- src/dictionaries/vi/docs.ts
+++ src/dictionaries/vi/docs.ts
@@ -1,28 +1,40 @@
 import { DocsDictionary } from "dictionaries/types/DocsDictionary";
 
 export const DocsLang: DocsDictionary = {
-   none: 'None',
-    title : 'Tài liệu',
-    button: {
-        add: 'Thêm'
-     },
-     filter: {
-        all : 'Tất cả',
-        fields  : 'Lĩnh vực',
-        search: 'Tìm kiếm tài liệu',
-        group : {
-            group : 'Nhóm',
-           none : 'Không',
-           creator : 'Người sáng tạo',
-           project : 'Dự án',
-        },
-        filter: {
-           filter : 'Lọc',
-           creator : 'Người sáng tạo',
-           lastEdited : 'Chỉnh sửa lần cuối',
-           name : 'Tên',
-           project : 'Dự án',
-           projectStatus : 'Trạng thái dự án',
-        }
-     }
+  none: "None",
+  title: "Tài liệu",
+  button: {
+    add: "Thêm",
+  },
+  filter: {
+    all: "Tất cả",
+    fields: "Lĩnh vực",
+    search: "Tìm kiếm tài liệu",
+    group: {
+      group: "Nhóm",
+      none: "Không",
+      creator: "Người sáng tạo",
+      project: "Dự án",
+    },
+    filter: {
+      filter: "Lọc",
+      creator: "Người sáng tạo",
+      lastEdited: "Chỉnh sửa lần cuối",
+      name: "Tên",
+      project: "Dự án",
+      projectStatus: "Trạng thái dự án",
+    },
+  },
+  createDoc: {
+    share: "Chia sẻ",
+    copyLink: "Sao chép Link",
+    comment: "Bình luận",
+    slider: "Mở slider",
+    more: "Thêm",
+    history: "Lịch sử phiên bản",
+    text: "Tùy chọn văn bản",
+    Ltext: "Văn bản lớn",
+    full: "Chiều rộng đầy đủ",
+    restore :"Khôi phục"
+  },
 };
