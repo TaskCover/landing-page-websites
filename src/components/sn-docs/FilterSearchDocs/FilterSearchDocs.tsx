5b6b03024cc96d11cf1a7ac7c658e04696d5d398
Le Phu <tui.hrv@gmail.com>
2023-12-14 23:58:28 +0700

    update UI filter docs

--- src/components/sn-docs/FilterSearchDocs/FilterSearchDocs.tsx
+++ src/components/sn-docs/FilterSearchDocs/FilterSearchDocs.tsx
@@ -33,86 +33,20 @@ const FilterSearchDocs = ({ onChange, queries }: FilterSearchDocsProps) => {
 
   return (
     <>
-      <Box
-        onClick={(e) => setAnchorEl(e.currentTarget)}
-        sx={{
-          color: "grey.400",
-          fontWeight: 600,
-          height: 32,
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          cursor: "pointer",
-        }}
-      >
-        <Text color={"gray.400"} fontWeight={600} fontSize={"14px"}>
-          {docsT("filter.filter.filter")}
-        </Text>
-        <Box
-          sx={{
-            color: isHasValue ? "primary.main" : "grey.400",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <ChevronIcon fontSize="small"></ChevronIcon>
-        </Box>
-      </Box>
-      <Popover
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        sx={{
-          [`& .${popoverClasses.paper}`]: {
-            backgroundImage: "none",
-            minWidth: 150,
-            maxWidth: 150,
-          },
-        }}
-        slotProps={{
-          paper: {
-            sx: {
-              borderRadius: 1,
-            },
-          },
-        }}
-      >
-        <Stack
-          sx={{
-            boxShadow: "2px 2px 24px rgba(0, 0, 0, 0.1)",
-            border: "1px solid",
-            borderTopWidth: 0,
-            borderColor: "grey.100",
-            borderRadius: 1,
-          }}
-        >
-          <MenuList component={Box} sx={{ py: 1 }}>
-            <FilterMember queries={queries} onChange={onChange}></FilterMember>
-            <FilterMemberEdit
-              queries={queries}
-              onChange={onChange}
-            ></FilterMemberEdit>
-            {/* <FilterName queries={queries} onChange={onChange}></FilterName> */}
-            <FilterMemberProject
-              queries={queries}
-              onChange={onChange}
-            ></FilterMemberProject>
-            <FilterProjectStatus
-              queries={queries}
-              onChange={onChange}
-            ></FilterProjectStatus>
-          </MenuList>
-        </Stack>
-      </Popover>
+      <FilterMember queries={queries} onChange={onChange}></FilterMember>
+      <FilterMemberEdit
+        queries={queries}
+        onChange={onChange}
+      ></FilterMemberEdit>
+      {/* <FilterName queries={queries} onChange={onChange}></FilterName> */}
+      <FilterMemberProject
+        queries={queries}
+        onChange={onChange}
+      ></FilterMemberProject>
+      <FilterProjectStatus
+        queries={queries}
+        onChange={onChange}
+      ></FilterProjectStatus>
     </>
   );
 };
--- src/components/sn-projects/components/SelectMembers.tsx
+++ src/components/sn-projects/components/SelectMembers.tsx
@@ -36,7 +36,7 @@ const SelectMembers = (props: SelectMembersProps) => {
   const ignoreItems = useMemo(() => {
     if (!ignoreId) return items;
     return items.filter((item) => item.id !== ignoreId);
-  }, [ignoreId, items]);
+  }, [ignoreId, items,]);
 
   const ignoreMembers = useMemo(() => {
     if (!ignoreId) return members;
@@ -79,81 +79,12 @@ const SelectMembers = (props: SelectMembersProps) => {
 
   useEffect(() => {
     onGetOptions({ pageIndex: 1, pageSize: 200000 });
+    onGetEmployeeOptions({ pageIndex: 1, pageSize: 20 });
   }, [onGetOptions]);
 
   return (
     <>
-      <Stack
-        direction="row"
-        py={1}
-        px={2.5}
-        bgcolor="grey.50"
-        justifyContent="space-between"
-        onClick={onOpen}
-        minHeight={56}
-        borderRadius={1}
-        sx={{ cursor: "pointer" }}
-      >
-        <Stack flex={1} spacing={0.5}>
-          <Text variant="caption" color="grey.300">
-            {projectT("list.form.title.members")}
-          </Text>
-          <Stack
-            direction="row"
-            rowGap={1.5}
-            columnGap={1.5}
-            flex={1}
-            flexWrap="wrap"
-          >
-            {ignoreMembers?.map((member) => (
-              <DisplayItem
-                key={member.id}
-                {...member}
-                onRemove={onRemoveMember}
-              />
-            ))}
-          </Stack>
-        </Stack>
-
-        <ChevronIcon
-          sx={{ color: "grey.400", fontSize: 16, alignSelf: "center" }}
-        />
-      </Stack>
-      <Popover
-        id={popoverId}
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={onClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "right",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        sx={{
-          [`& .${popoverClasses.paper}`]: {
-            backgroundImage: "none",
-            width: anchorEl?.offsetWidth ?? 200,
-            maxHeight: 350,
-            boxShadow: "2px 2px 24px rgba(0, 0, 0, 0.1)",
-            border: "1px solid",
-            borderColor: "grey.100",
-            borderBottomLeftRadius: 1,
-            borderBottomRightRadius: 1,
-          },
-        }}
-        slotProps={{
-          paper: {
-            sx: {
-              borderRadius: 1,
-              mt: 0.5,
-            },
-          },
-        }}
-      >
-        <Stack p={2}>
+      <Stack p={2}>
           <Search
             name="email"
             value={filters?.email}
@@ -175,7 +106,6 @@ const SelectMembers = (props: SelectMembersProps) => {
             })}
           </MenuList>
         </Stack>
-      </Popover>
     </>
   );
 };
