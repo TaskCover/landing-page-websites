5b3b18458f262a4a7c827cd61a67cca46872f460
Nhan Phan <phnhan0301@hotmail.com>
2023-12-31 15:54:26 +0700

    style: #23 make task title in breadcrumbs one line with truncate

--- src/components/sn-project-detail/Tasks/Detail/index.tsx
+++ src/components/sn-project-detail/Tasks/Detail/index.tsx
@@ -1,24 +1,31 @@
-import { memo, useEffect, useState, useRef, useMemo } from "react";
-import { DrawerProps, Drawer, drawerClasses, Stack, Box, Typography, Breadcrumbs, OutlinedInput } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "store/hooks";
-import { IconButton, Input, Text } from "components/shared";
-import { useTranslations } from "next-intl";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import {
+  Box,
+  Breadcrumbs,
+  Drawer,
+  Stack,
+  Typography,
+  drawerClasses,
+} from "@mui/material";
+import { IconButton } from "components/shared";
 import { NS_PROJECT } from "constant/index";
 import CloseIcon from "icons/CloseIcon";
-import TabList, { TabDetail } from "./TabList";
-import Information from "./Information";
-import { Task } from "store/project/reducer";
-import Comments from "./Comments";
+import { useTranslations } from "next-intl";
+import { memo, useMemo, useRef, useState } from "react";
+import { useAppSelector } from "store/hooks";
 import { useTaskDetail } from "store/project/selectors";
-import { AssignTask, CommentEditor, StatusTask } from "./components";
 import Activities from "./Activities";
+import Comments from "./Comments";
+import Information from "./Information";
+import TabList, { TabDetail } from "./TabList";
+import { AssignTask, CommentEditor, StatusTask } from "./components";
 import EditTask from "./components/EditTask";
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Actions, { Action } from "./components/SubTasksOfTask/Actions";
 import MoveOtherTask from "./components/SubTasksOfTask/MoveOtherTask";
 
 const Detail = () => {
-  const { task, taskParent, onUpdateTaskDetail, onUpdateTask } = useTaskDetail();
+  const { task, taskParent, onUpdateTaskDetail, onUpdateTask } =
+    useTaskDetail();
   const taskLists = useAppSelector((state) => state.project.tasks);
   const scrollEndRef = useRef<HTMLDivElement | null>(null);
   const [action, setAction] = useState<Action | undefined>();
@@ -36,7 +43,7 @@ const Detail = () => {
 
   const [tab, setTab] = useState<TabDetail>(TabDetail.DETAIL);
   const onClose = () => {
-    setTab(TabDetail.DETAIL)
+    setTab(TabDetail.DETAIL);
     onUpdateTaskDetail();
   };
 
@@ -50,15 +57,21 @@ const Detail = () => {
   }
 
   const onGotoTask = async () => {
-    const taskList = taskLists.filter(item => item.id === task.taskListId)
+    const taskList = taskLists.filter((item) => item.id === task.taskListId);
 
-    if (!taskList[0]) return
+    if (!taskList[0]) return;
 
-    const taskOfSubtask = taskList[0].tasks.filter(item => item.id === task.taskId)
+    const taskOfSubtask = taskList[0].tasks.filter(
+      (item) => item.id === task.taskId,
+    );
 
-    if (!taskOfSubtask[0]) return
+    if (!taskOfSubtask[0]) return;
 
-    onUpdateTaskDetail({ ...taskOfSubtask[0], taskId: taskOfSubtask[0].id, taskListId: taskList[0].id });
+    onUpdateTaskDetail({
+      ...taskOfSubtask[0],
+      taskId: taskOfSubtask[0].id,
+      taskListId: taskList[0].id,
+    });
   };
 
   const breadcrumbs = breadcrumbs_values.map((item, idx) => {
@@ -67,17 +80,21 @@ const Detail = () => {
         key={idx + 1}
         color="text.primary"
         sx={{
-          fontSize: '20px',
-          fontWeight: idx !== 2 ? '500' : '400',
-          color: idx === 0 ? 'grey.300' : 'text.primary',
-          cursor: idx === 1 ? 'pointer' : 'unset'
+          fontSize: "20px",
+          fontWeight: idx !== 2 ? "500" : "400",
+          color: idx === 0 ? "grey.300" : "text.primary",
+          cursor: idx === 1 ? "pointer" : "unset",
+          textOverflow: "ellipsis",
+          textWrap: "nowrap",
+          display: "block",
+          overflow: "hidden",
         }}
         onClick={idx === 1 ? onGotoTask : undefined}
       >
         {item}
       </Typography>
-    )
-  })
+    );
+  });
 
   const onChangeAction = (newAction?: Action) => {
     setAction(newAction);
@@ -89,7 +106,7 @@ const Detail = () => {
 
   const onAfterSubmit = () => {
     onUpdateTaskDetail();
-  }
+  };
 
   return (
     <Drawer
@@ -116,19 +133,40 @@ const Detail = () => {
           <Breadcrumbs
             separator={<NavigateNextIcon color="disabled" fontSize="small" />}
             aria-label="breadcrumb"
+            sx={{
+              ".MuiBreadcrumbs-li": {
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                flex: 1,
+
+                "&:first-child": {
+                  flex: "0 auto",
+                },
+              },
+            }}
           >
             {breadcrumbs}
           </Breadcrumbs>
-          {task.subTaskId &&
+          {task.subTaskId && (
             <Stack ml={2}>
-              <Actions subId={task.subTaskId} onChangeAction={onChangeAction} options={options} />
+              <Actions
+                subId={task.subTaskId}
+                onChangeAction={onChangeAction}
+                options={options}
+              />
 
               {/* ACTIONS */}
               {action === Action.CHANGE_PARENT_TASK && (
-                <MoveOtherTask subId={task.subTaskId} open onClose={onResetAction} onAfterSubmit={onAfterSubmit} />
+                <MoveOtherTask
+                  subId={task.subTaskId}
+                  open
+                  onClose={onResetAction}
+                  onAfterSubmit={onAfterSubmit}
+                />
               )}
             </Stack>
-          }
+          )}
         </Stack>
         <IconButton onClick={onClose}>
           <CloseIcon />
