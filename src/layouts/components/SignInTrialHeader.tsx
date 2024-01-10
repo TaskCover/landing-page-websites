33c99bbc6eec27bd4d8192cb979d669b6aaf927e
Manh Pham(Harding)/Sotatek/FE_MnF <131767799+HardingPhamSotatek@users.noreply.github.com>
2024-01-10 10:10:15 +0700

    update task cover

--- src/layouts/components/SignInTrialHeader.tsx
+++ src/layouts/components/SignInTrialHeader.tsx
@@ -5,7 +5,7 @@ import { memo } from "react";
 const SignInTrialHeader = () => {
     return(
         <Stack direction="row" spacing={2}>
-            <Button className="MuiButton-primaryOutlined" sx={{p:"8px 12px"}}>Log in</Button>
+            <Button className="MuiButton-primaryOutlined" sx={{p:"8px 12px"}} href="http://103.182.16.241:2023/">Log in</Button>
             <Button className="MuiButton-primary" sx={{p:"8px 12px"}}>Free 14-day trial</Button>
         </Stack>
     );
