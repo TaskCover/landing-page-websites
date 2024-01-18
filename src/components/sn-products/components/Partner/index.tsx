c6e93082908837dede15362b59a151beb64119ca
Manh Pham(Harding)/Sotatek/FE_MnF <131767799+HardingPhamSotatek@users.noreply.github.com>
2024-01-18 17:35:49 +0700

    update partner

--- src/components/sn-products/components/Partner/index.tsx
+++ src/components/sn-products/components/Partner/index.tsx
@@ -4,32 +4,67 @@ import { Text } from "components/shared";
 import useBreakpoint from "hooks/useBreakpoint";
 
 export const Partner = () => {
-    const {isMdSmaller} = useBreakpoint();
+  const { isMdSmaller } = useBreakpoint();
   return (
     <Stack position="relative" mt={4}>
-      <Text fontWeight={500} fontSize={{md: 40, xs: 24}} my={{md: 8, xs: 3}} textAlign="center">
+      <Text
+        fontWeight={500}
+        fontSize={{ md: 40, xs: 24 }}
+        my={{ md: 8, xs: 3 }}
+        textAlign="center"
+      >
         Partner
       </Text>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        flexWrap="wrap"
-      >
-        {DATA.map((data, index) => (
-          <Stack key={index} mb={2} width={{lg: "auto", md: "20%", sm: "30%", xs: "40%"}}>
-            <Image
-              src={data}
-              width={isMdSmaller ? 136 : 0}
-              height={0}
-              sizes="100vw"
-              style={{
-                width: "100%",
-                height: "auto",
-              }}
-              alt="logo-partner"
-            />
-          </Stack>
-        ))}
+      <Stack direction="row" justifyContent="center" flexWrap="wrap">
+        {DATA.map((data, index) =>
+          isMdSmaller ? (
+            (index + 3) % 3 === 0 ? (
+              <Stack width="100%" key={index} alignItems="center">
+                <Stack mb={2} width={{ md: "20%", xs: "40%" }}>
+                  <Image
+                    src={data}
+                    width={isMdSmaller ? 136 : 0}
+                    height={0}
+                    sizes="100vw"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                    }}
+                    alt="logo-partner"
+                  />
+                </Stack>
+              </Stack>
+            ) : (
+              <Stack key={index} mb={2} width={{ md: "20%", xs: "40%" }}>
+                <Image
+                  src={data}
+                  width={isMdSmaller ? 136 : 0}
+                  height={0}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  alt="logo-partner"
+                />
+              </Stack>
+            )
+          ) : (
+            <Stack key={index} mb={2} width="20%">
+              <Image
+                src={data}
+                width={isMdSmaller ? 136 : 0}
+                height={0}
+                sizes="100vw"
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
+                alt="logo-partner"
+              />
+            </Stack>
+          )
+        )}
       </Stack>
     </Stack>
   );
